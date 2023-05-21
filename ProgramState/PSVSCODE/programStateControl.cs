c5290d3fe790e6d431b492241db772250cc8f8bb
hasan-kayan <hasankayan2000@hotmail.com>
2023-05-21 21:34:45 +0300

    Process VSCode Started

---
+++ ProgramState/PSVSCODE/programStateControl.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string processName = "notepad";
+        bool isRunning = IsProcessRunning(processName);
+
+        if (isRunning)
+        {
+            Console.WriteLine($"{processName} çalışıyor.");
+
+            Process[] processes = Process.GetProcessesByName(processName);
+            bool isNotResponding = !processes[0].Responding;
+            bool isSuspended = false;
+
+            foreach (ProcessThread thread in processes[0].Threads)
+            {
+                if (thread.ThreadState == ThreadState.Wait || thread.ThreadState == ThreadState.Suspended)
+                {
+                    isSuspended = true;
+                    break;
+                }
+            }
+
+            if (isNotResponding)
+            {
+                Console.WriteLine($"{processName} yanıt vermiyor.");
+            }
+            else
+            {
+                Console.WriteLine($"{processName} yanıt veriyor.");
+            }
+
+            if (isSuspended)
+            {
+                Console.WriteLine($"{processName} askıya alınmış durumda.");
+            }
+            else
+            {
+                Console.WriteLine($"{processName} askıda değil.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{processName} çalışmıyor.");
+        }
+    }
+
+    static bool IsProcessRunning(string processName)
+    {
+        Process[] processes = Process.GetProcessesByName(processName);
+        return processes.Length > 0;
+    }
+}
