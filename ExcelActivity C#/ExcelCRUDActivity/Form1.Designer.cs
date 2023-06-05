88af189bede1ff2f5f0765fa0264a8a0873aa2fd
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-05 09:49:23 +0300

    New approaches

--- ExcelActivity C#/ExcelCRUDActivity/Form1.Designer.cs
+++ ExcelActivity C#/ExcelCRUDActivity/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -38,19 +39,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(207, 54);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Choose File";
+            this.button1.Text = "Read";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Choose File";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -62,6 +73,7 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ExcelActivity C#/ExcelCRUDActivity/Program.cs
+++ ExcelActivity C#/ExcelCRUDActivity/Program.cs
@@ -1,46 +1,89 @@
 ﻿using System;
 using System.Data;
+using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 
-namespace ExcelActivities
+class Program
 {
-    class Program
+    static void Main()
     {
-        public static void ReadExcelFile(string filePath)
+        // Get Excel file path from the user
+        Console.WriteLine("Enter the Excel file path:");
+        string filePath = Console.ReadLine();
+
+        // Create an Excel application instance
+        Excel.Application excelApp = new Excel.Application();
+
+        try
         {
-            Excel.Application excelApp = new Excel.Application();
+            // Open the Excel workbook
             Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
-            Excel.Worksheet worksheet = workbook.ActiveSheet; // Use the active sheet
 
-            Excel.Range range = worksheet.UsedRange;
+            // Get the first worksheet
+            Excel.Worksheet worksheet = workbook.Sheets[1];
 
-            // Iterate through each cell in the range
-            foreach (Excel.Range cell in range)
+            // Get the used range of the worksheet
+            Excel.Range usedRange = worksheet.UsedRange;
+
+            // Convert the used range to a DataTable
+            DataTable dataTable = ConvertRangeToDataTable(usedRange);
+
+            // Display the DataTable (optional)
+            Console.WriteLine("DataTable contents:");
+            foreach (DataRow row in dataTable.Rows)
             {
-                string cellValue = cell.Value2?.ToString(); // Get the value of the cell
-
-                // Do something with the cell value
-                Console.WriteLine("Cell [{0},{1}]: {2}", cell.Row, cell.Column, cellValue);
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString() + "\t");
+                }
+                Console.WriteLine();
             }
 
-            // Close Excel objects and release resources
+            // Write the DataTable back to the worksheet starting from cell A1
+            Excel.Range targetRange = worksheet.Range["A1"].Resize[dataTable.Rows.Count, dataTable.Columns.Count];
+            targetRange.Value = dataTable;
+
+            // Save and close the workbook
+            workbook.Save();
             workbook.Close();
+
+            Console.WriteLine("DataTable written to Excel successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            // Clean up the Excel application
             excelApp.Quit();
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
-            worksheet = null;
-            workbook = null;
-            excelApp = null;
-            GC.Collect();
+            Marshal.ReleaseComObject(excelApp);
         }
 
-        static void Main(string[] args)
+        Console.ReadLine();
+    }
+
+    static DataTable ConvertRangeToDataTable(Excel.Range range)
+    {
+        DataTable dataTable = new DataTable();
+
+        // Add columns to the DataTable
+        foreach (Excel.Range column in range.Columns)
         {
-            Console.WriteLine("Please Enter Excel Path You Want to Work on:");
-            string ExcelPath = Console.ReadLine();
-            ReadExcelFile(ExcelPath);
-            
+            dataTable.Columns.Add(column.Value.ToString());
         }
+
+        // Add rows to the DataTable
+        for (int row = 2; row <= range.Rows.Count; row++)
+        {
+            DataRow dataRow = dataTable.NewRow();
+            for (int col = 1; col <= range.Columns.Count; col++)
+            {
+                dataRow[col - 1] = range.Cells[row, col].Value;
+            }
+            dataTable.Rows.Add(dataRow);
+        }
+
+        return dataTable;
     }
 }
