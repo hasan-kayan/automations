99d737f0245495b31c1056b3297ad92ff7cb6d56
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-05 11:42:08 +0300

    Building Steps Microsoft.Office.Interop.Excel and interface

--- ExcelActivity C#/ExcelApplications/Program.cs
+++ ExcelActivity C#/ExcelApplications/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Automaiton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start");
+            Application app = new Application();
+            app.Visible = true;
+
+
+        }
+    }
+}
